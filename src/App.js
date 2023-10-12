00c9f79b47df138403d667e1741b3a66bf02a640
Divy <divypatel417@gmail.com>
2023-10-12 12:02:58 +1030

    particals

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Introduction from "./components/Introduction";
 import Links from "./components/Links";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 import Projects from "./components/Projects";
 import Routing from "./components/Routing";
 
@@ -10,11 +13,95 @@ function App() {
     console.log("in function");
     document.getElementById(pos).scrollIntoView();
   };
-
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
   return (
     <div className=" font-sans bg-darkblue">
+          <Particles
+           init={particlesInit}
+        options={{
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#5eead4',
+            },
+            lineLinked: {
+              color: {
+                value: '#5eead4',
+              },
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: true,
+              speed: 3,
+              straight: true,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 1500,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.1,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}>
+      </Particles>  
+
       <div className=" lg:px-48 sm:px-6 lg:h-screen flex lg:flex-row sm:flex-col gap-2 sm:overflow-scroll sm:h-screen no-scrollbar">
+     
+    
         <div className=" lg:pt-20 sm:pt-20 flex flex-col w-1/2 sm:w-full lg:justify-between sm:justify-normal pb-20">
+          
           <div>
             <Introduction></Introduction>
 
